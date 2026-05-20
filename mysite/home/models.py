945e52df3d9e8f0f34b619f976c5704782c9e365
Ir0nblast3 <tpafonso1@gmail.com>
2026-05-20 12:57:53 +0100

    adding details

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 
@@ -7,4 +7,4 @@ from wagtail.fields import RichTextField
 class HomePage(Page):
     body = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + ["body"]
+    content_panels = Page.content_panels +  [FieldPanel('body'),]
